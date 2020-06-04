e807a893a3092ec67c6064816a11bcfed5e01088
Leon <leondevlifelog@gmail.com>
2020-06-04 16:33:55 +0800

    Update build-android.sh

--- build-android.sh
+++ build-android.sh
@@ -5,3 +5,4 @@ cd openssl
 PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
 ./Configure android-${ARCH} -D__ANDROID_API__=21
 make
+ls
